82cd7a2bc5b46f00be32753ae4ea7f7d8bfee218
Asyraf Hussin <asyrafhussin4@gmail.com>
2017-09-26 00:04:44 +0800

    Add serve command port options

--- Bright/Console/ServeCommand.php
+++ Bright/Console/ServeCommand.php
@@ -6,6 +6,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
 
 final class ServeCommand extends Command
 {
@@ -16,6 +17,11 @@ final class ServeCommand extends Command
     {
         $this->setName('serve');
         $this->setDescription('Serve the application on the PHP development server.');
+        $this->addOption('port',
+                          null,InputOption::VALUE_REQUIRED,
+                          'Development Server port listening',
+                          8000
+                        );
     }
 
     /**
@@ -23,8 +29,8 @@ final class ServeCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // default port
-        $port = 8000;
+        // get port
+        $port = $input->getOption('port');
 
         echo "\033[33m Bright Development Server started \n \033[0mListening on http://localhost:".$port."\n";
         shell_exec('php -S localhost:'.$port.' public/index.php');
