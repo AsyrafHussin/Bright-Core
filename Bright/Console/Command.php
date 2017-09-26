0bc38875224b590a49ffaf8b2368a7086ae255e5
Asyraf Hussin <asyrafhussin4@gmail.com>
2017-09-26 08:44:44 +0800

    Add comment to command core file

--- Bright/Console/Command.php
+++ Bright/Console/Command.php
@@ -10,8 +10,19 @@ use \Bright\Console\ServeCommand;
  */
 class Command
 {
+    /**
+     * Parameters for console application
+     * @var object
+     */
     protected $consoleApplication = null;
     
+    /**
+     * Class constructor
+     *
+     * @param object $consoleApplication Parameters for console application
+     *
+     * @return void
+     */
     public function __construct()
     {
         $name = "Bright Framework";
@@ -19,11 +30,21 @@ class Command
         $this->consoleApplication = new Application($name,$version);
     }
 
+    /**
+     * Register all commands for console application
+     *
+     * @return void
+     */
     public function registerCommand()
     {
         $this->consoleApplication->add(new ServeCommand());
     }
 
+    /**
+     * Run console application
+     *
+     * @return void
+     */
     public function run()
     {
         $this->consoleApplication->run();
